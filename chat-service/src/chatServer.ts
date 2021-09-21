11d40f369496062f60f2b8ac54e267737a0a211b
yhung-mea7 <60412916+yhung-mea7@users.noreply.github.com>
2021-09-21 17:18:02 -0600

    socket accepts client connections

--- chat-service/src/chatServer.ts
+++ chat-service/src/chatServer.ts
@@ -1,15 +1,15 @@
-import express, { Application } from "express";
-import http from "http";
-import { Server, Socket } from "socket.io";
-import redisAdapter from "@socket.io/redis-adapter";
-import redis from "redis";
-import url from "url";
-import { IProperty } from "./model";
+import express, { Application } from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import redisAdapter from '@socket.io/redis-adapter';
+import redis from 'redis';
+import url from 'url';
+import { IProperty } from './model';
 import {
   newConsulDetails,
   registerService,
   deregisterService,
-} from "./register/register.js";
+} from './register/register.js';
 
 export class ChatServer {
   public static readonly PORT: number = 8080;
@@ -22,7 +22,16 @@ export class ChatServer {
     this.app = express();
     this.port = process.env.PORT || ChatServer.PORT;
     this.server = http.createServer(this.app);
-    this.io = new Server({ path: "/" }).listen(this.server);
+    this.io = new Server({
+      path: '/',
+      transports: ['websocket','polling'],
+      cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        credentials: true,
+      },
+      allowEIO3: true,
+    }).listen(this.server);
 
     const pubClient = redis.createClient({
       host: process.env.REDIS_HOST,
@@ -32,27 +41,31 @@ export class ChatServer {
     this.io.adapter(redisAdapter.createAdapter(pubClient, subClient));
 
     this.server.listen(this.port, () => {
-      var details = newConsulDetails("chat-service", <number>this.port);
+      var details = newConsulDetails('chat-service', <number>this.port);
       registerService(details);
       console.log(`server is running on port ${this.port}`);
 
-      process.on("SIGTERM", () => {
+      process.on('SIGTERM', () => {
         deregisterService(<string>details.id);
         process.exitCode = 1;
       });
     });
 
     const ns = this.io.of(/\/\w+/);
-    ns.on("connection", (socket: Socket) => {
+    ns.on('connection', (socket: Socket) => {
+      console.log(socket.handshake.url)
       var room = url.parse(socket.handshake.url, true).query.room;
+      console.log(room)
+      var ns = socket.nsp.name.replace(/[/]/g,"");
+      console.log(ns)
 
-      if (typeof room !== "string") {
-        socket.disconnect(true);
-      }
+      // if (typeof room !== 'string') {
+      //   socket.disconnect(true);
+      // }
       var validNs = props.filter(
         (p) =>
-          p.serverCode === socket.nsp.name.substr(1) &&
-          p.Channels.includes(room as string)
+          p.serverCode === ns &&
+          p.Channels.includes(room as string),
       );
       if (validNs.length !== 1) {
         socket.disconnect(true);
@@ -60,11 +73,15 @@ export class ChatServer {
       }
       socket.join(room as string);
       console.log(
-        `socket joined room ${room} in namespace ${socket.nsp.name.substr(1)}`
+        `socket joined room ${room} in namespace ${socket.nsp.name.substr(1)}`,
       );
+      socket.on('msg', (msg) => {
+        console.log(msg);
+        socket.broadcast.emit('msg', msg);
+      });
 
-      socket.on("disconnect", (reason: string) => console.log(reason));
-      socket.on("error", (err: Error) => {
+      socket.on('disconnect', (reason: string) => console.log(reason));
+      socket.on('error', (err: Error) => {
         console.log(err);
         socket.leave(room as string);
         socket.disconnect(true);
